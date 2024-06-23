dce2200795777bb81c48e25e7cde5e99b907cc80
Pratik Prajapati <96768526+Pratik-7i@users.noreply.github.com>
2024-06-23 12:06:39 +0530

    Initial Commit

---
+++ Planets/PlanetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlanetsApp.swift
+//  Planets
+//
+//  Created by Pratik on 23/06/24.
+//
+
+import SwiftUI
+
+@main
+struct PlanetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
